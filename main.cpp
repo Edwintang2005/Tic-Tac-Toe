c551f66ab4fdfe648b2710f1a49090d94c5565bf
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2024-05-16 10:58:19 +1000

    Completed functionality and added board visualisation

--- main.cpp
+++ main.cpp
@@ -9,6 +9,59 @@ This will be a tic-tac-toe game that runs on the c++ framework.
 // Using the standard namespace (scope of the program)
 using namespace std;
 
+
+
+// Function prototype for all functions
+void playAgain();
+bool checkplot(int row, int col, char board[3][3]);
+bool checkwin(char board[3][3], int player);
+void printBoard(char board[3][3]);
+
+int main() {
+    // Initialising the board for the game
+    char board[3][3] = {{' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}};
+    int player = 1;
+    int row, col;
+    int turn;
+    
+    for (turn = 0; turn < 9; turn++) {
+        // Setting up input for each turn
+        while (1) {
+            // cout is the standard output function in c++
+            cout << "Player number " << player << ", choose your plot!\n";
+            cout << "Row (Enter from 1-3): ";
+            // cin is the standard input function in c++
+            cin >> row;
+            cout << "Column (Enter from 1 to 3): ";
+            cin >> col;
+            if (checkplot(row, col, board)) {
+                break;
+            } else {
+                cout << "Invalid plot!\n";
+            }
+        }
+        // Updating the board with the player's character
+        board[row - 1][col - 1] = (char) player;
+        
+        printBoard(board);
+
+        // Checking if a player has won
+        if (checkwin(board, player)) {
+            cout << "Player " << player << " Wins this game!\n";
+            playAgain();
+            return 0;
+        }
+        // Statement to switch player to 2 if equal to 1, 1 if equal to 2
+        player = (player == 1) ? 2:1;
+    }
+    cout << "Draw, no one wins!\n";
+    playAgain();
+    return 0;
+}
+
+// A function that checks to make sure that the plot inputted is valid
 bool checkplot(int row, int col, char board[3][3]) {
     if (row <= 0 || row > 3) {
         return false;
@@ -20,6 +73,7 @@ bool checkplot(int row, int col, char board[3][3]) {
     return true;
 }
 
+// A function that checks all three in a rows to determine if player has won
 bool checkwin(char board[3][3], int player) {
     // Checking rows and columns
     for (int i = 0; i < 3; i++) {
@@ -38,50 +92,26 @@ bool checkwin(char board[3][3], int player) {
     }
     return false;
 }
-void playAgain();
 
-
-int main() {
-    // Initialising the board for the game
-    char board[3][3] = {{' ', ' ', ' '},
-                        {' ', ' ', ' '},
-                        {' ', ' ', ' '}};
-    int player = 1;
-    int row, col;
-    int turn;
-    
-    for (turn = 0; turn < 9; turn++) {
-        // Setting up input for each turn
-        while (1) {
-            // cout is the standard output function in c++
-            cout << "Player number " << player << ", choose your plot!\n";
-            cout << "Row (Enter from 1-3):";
-            // cin is the standard input function in c++
-            cin >> row;
-            cout << "Column (Enter from 1 to 3):";
-            cin >> col;
-            if (checkplot(row, col, board)) {
-                break;
+// A function that prints out the game board
+void printBoard(char board[3][3]) {
+    cout << "-------\n";
+    for (int i = 0; i < 3; i++) {
+        cout << "|";
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == (char)1) {
+                cout << "X|";
+            } else if (board[i][j] == (char)2) {
+                cout << "O|";
             } else {
-                cout << "Invalid plot!\n";
+                cout << " |";
             }
         }
-        // Updating the board with the player's character
-        board[row - 1][col - 1] = (char) player;
-        // Checking if a player has won
-        if (checkwin(board, player)) {
-            cout << "Player " << player << " Wins this game!\n";
-            playAgain();
-            return 0;
-        }
-        // Statement to switch player to 2 if equal to 1, 1 if equal to 2
-        player = (player == 1) ? 2:1;
+        cout << "\n-------\n";
     }
-    cout << "Draw, no one wins!\n";
-    playAgain();
-    return 0;
 }
 
+// A function that allows player to play again by recalling the main function
 void playAgain() {
     char cont = 'M';
     cout << "Play again? (Y/N)\n";
